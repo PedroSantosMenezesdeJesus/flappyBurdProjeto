e3dea6e1053cf8e63e822feaa059782493a6cb95
PedroSantosMenezesdeJesus <pedro.jesus63@etec.sp.gov.br>
2024-08-08 12:21:39 -0300

    salvar

--- flappyBird/js/class.js
+++ flappyBird/js/class.js
@@ -1,3 +1,24 @@
 class obj {
-    
+    constructor(px,py,lar,alt,imagem){
+        this.px = px
+        this.py = py
+        this.lar = lar
+        this.alt = alt
+        this.imagem = imagem
+    }
+    draw(){
+        let img = new Image()
+        img.src = this.imagem
+        canvas.drawImage(img,this.px,this.py,this.lar,this.alt)
+        
+    }
+}
+
+class BG extends obj{
+    move(velocidade, limite, pos){
+        this.px -= velocidade
+        if (this.px < -limite) {
+            this.px = pos 
+        }
+    }
 }
--- flappyBird/js/script.js
+++ flappyBird/js/script.js
@@ -1,2 +1,23 @@
-const canvas = document.getElementById('canvas').getContext('2d')
+let canvas = document.getElementById('canvas').getContext('2d')
 // indica um elemento 2d const quadro = canvas.getContext('2d')
+
+let fundo = new BG(0,0,500,900,'img/sky.png')
+let fundo2 = new BG(500, 0, 500, 900, 'img/sky.png')
+
+function draw(){
+    fundo.draw()
+    fundo2.draw()
+}
+
+function update(){
+    fundo.move(7, 500, 0)
+    fundo2.move(7, 0, 500)
+}
+
+function main(){
+    canvas.clearRect(0,0,500,900)
+    update()
+    draw()
+}
+
+setInterval(main,100)
